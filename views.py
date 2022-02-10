c97e165ffabd48bfd13b76a68c43c58f956d7c5a
PR1 <511242@mail.ru>
2022-02-10 13:51:19 +0300

    view

---
+++ views.py
@@ -0,0 +1,19 @@
+
+
+class Index:
+    def __call__(self, request):
+        return '200 OK', render(
+            'index.html', date=request.get('date', None))
+
+
+class Contact:
+    def __call__(self, request):
+        return '200 OK', render(
+            'contact.html', date=request.get('date', None))
+
+
+class Page:
+    def __call__(self, request):
+        return '200 OK', render(
+            'contact.html', date=request.get('date', None))
+
