c0ee9255cb8462a66a8cedfff3bae59793f47295
PR1 <511242@mail.ru>
2022-02-28 14:19:07 +0300

    homework by lesson 5

---
+++ examples_5/class_adapter.py
@@ -0,0 +1,59 @@
+from abc import ABCMeta, abstractmethod
+from math import sqrt
+
+"""
+Структурный паттерн - Адаптер:
+«Преобразует интерфейс одного класса в 
+интерфейс другого, который ожидают клиенты»
+
+Адаптеры бывают двух типов: адаптер класса и адаптер объекта.
+
+классы круг и квадрат с разными интерфейсами
+нужно ссоздать класс-адаптер
+"""
+
+
+# нечто круглое, имеющее радиус
+class Roundable(metaclass=ABCMeta):
+    @abstractmethod
+    def get_radius(self):
+        pass
+
+
+# конкретный класс - окружность - имеет радиус
+class Circle(Roundable):
+    def __init__(self, radius):
+        self._radius = radius
+
+    def get_radius(self):
+        return self._radius
+
+
+# квадрат со стороной side
+class Square:
+    def __init__(self, side):
+        self._side = side
+
+    def get_side(self):
+        return self._side
+
+
+# круглый квадрат (вписанная окружность)
+class RoundableSquare(Square, Roundable):
+    """
+    это класс-адаптер
+    Square – класс, который нужно привести к новому интерфейсу. Адаптируемый класс.
+    Roundable – интерфейс, к которому мы хотим привести наш класс Square.
+    """
+    def get_radius(self):
+        return self.get_side() * sqrt(2) / 2
+
+
+circle_1 = Circle(5)
+roundable_square_1 = RoundableSquare(5)
+
+print(circle_1.get_radius())
+print(roundable_square_1.get_radius())
+
+print(issubclass(circle_1.__class__, Roundable))
+print(issubclass(roundable_square_1.__class__, Roundable))
