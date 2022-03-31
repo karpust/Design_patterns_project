668e83bba0c310c179b84549d3980c37045b7353
karpust <956277@mail.ru>
2022-03-31 20:59:09 +0300

    homework by lesson 9 is done

---
+++ Django-Flask-RabbitMQ/step2_orders_flask/main.py
@@ -0,0 +1,88 @@
+from flask import Flask, json, request
+import sqlite3
+import pika
+from settings import TABLE_NAME, DB_NAME
+
+app = Flask(__name__)
+connection = sqlite3.connect(DB_NAME, check_same_thread=False)
+cursor = connection.cursor()
+
+
+@app.route('/')
+def index():
+    statement = f"SELECT * FROM {TABLE_NAME}"
+    cursor.execute(statement)
+    result = cursor.fetchall()
+    print(result)
+    response = app.response_class(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/sale/', methods=['POST'])  # тк данные принимаем
+def sale():
+    data = request.form   # приняли словарь который передали в джанго
+    phone = data['phone']
+    price = data['price']
+    is_sale = True
+    status = 'DONE'
+    statement = f"INSERT INTO {TABLE_NAME}(phone,price,is_sale,status) values (?,?,?,?)"
+    cursor.execute(statement, (phone, price, is_sale, status))
+    response = app.response_class(
+        response=json.dumps({'STATUS': 'OK'}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/repair/', methods=['POST'])  # тк данные принимаем
+def repair():
+    data = request.form
+    phone = data['phone']
+    price = data['price']
+    is_sale = False
+    status = 'IN PROCESS'
+    statement = f"INSERT INTO {TABLE_NAME}(phone,price,is_sale,status) values (?,?,?,?)"
+    cursor.execute(statement, (phone, price, is_sale, status))
+
+    # Публикуем сообщение; код выполнится только если запущет rabbit
+    connection = pika.BlockingConnection(pika.ConnectionParameters(  # подключение к очереди
+        'rabbitmq'))  # localhost
+    channel = connection.channel()  # создание канала
+    channel.queue_declare(queue='repair')  # создание очереди
+    channel.basic_publish(exchange='',  # размещаем сообщения
+                          routing_key='repair',
+                          body=phone)
+    connection.close()  # закрыли подключение; теперь сообщение находится в очереди
+
+    response = app.response_class(
+        response=json.dumps({'STATUS': 'OK'}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/change/', methods=['POST'])
+def change_status():
+    data = request.form
+    phone = data['phone']
+    status = data['status']
+    print(status)
+    statement = f"UPDATE {TABLE_NAME} set status = ? where phone = ?"
+    cursor.execute(statement, (status, phone))
+
+    response = app.response_class(
+        response=json.dumps({'STATUS': 'OK'}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Django-Flask-RabbitMQ/step3_repair_rabbit/main.py
@@ -0,0 +1,28 @@
+import pika
+import random
+import time
+import requests
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))  # localhost
+channel = connection.channel()
+channel.queue_declare(queue='repair')
+
+
+def callback(ch, method, properties, body):
+    """
+    Обработка чтения из очереди
+    """
+    phone = body
+    # Начинаем процедуру ремонта
+    repair_time = random.randint(3, 40)
+    time.sleep(repair_time)
+    # После окончания отправляем запрос на обновление статуса заказа
+    requests.post('http://orders_flask:5000/change/', data={'phone': phone, 'status': 'DONE'})  # 'http://127.0.0.1:5000/change/'
+
+
+channel.basic_consume(queue='repair', on_message_callback=callback)
+# on_message_callback - ответ на сообщение из очереди ф-ей callback
+
+print('start')
+channel.start_consuming()
+# при запуске сервиса раббит получить сообщение из очереди и ответить на него
